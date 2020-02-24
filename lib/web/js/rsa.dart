79c335420398d77bf6d2a6bb774d37376f00fd06
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2020-02-23 23:02:31 -0500

    implemented all methods on web assembly

--- lib/web/js/rsa.dart
+++ lib/web/js/rsa.dart
@@ -4,9 +4,77 @@ library rsa;
 import 'package:js/js.dart';
 
 @JS()
-external RSAGenerate(
-  int bits,
-  Function(String error, dynamic result) callback,
+external RSADecryptOAEP(
+  String message,
+  String label,
+  String hashName,
+  String pkcs12,
+  String passphrase,
+  Function(String error, String result) callback,
+);
+
+@JS()
+external RSADecryptPKCS1v15(
+  String message,
+  String pkcs12,
+  String passphrase,
+  Function(String error, String result) callback,
+);
+
+@JS()
+external RSAEncryptOAEP(
+  String message,
+  String label,
+  String hashName,
+  String pkcs12,
+  String passphrase,
+  Function(String error, String result) callback,
+);
+
+@JS()
+external RSAEncryptPKCS1v15(
+  String message,
+  String pkcs12,
+  String passphrase,
+  Function(String error, String result) callback,
+);
+
+@JS()
+external RSASignPSS(
+  String message,
+  String hashName,
+  String pkcs12,
+  String passphrase,
+  Function(String error, String result) callback,
+);
+
+@JS()
+external RSASignPKCS1v15(
+  String message,
+  String hashName,
+  String pkcs12,
+  String passphrase,
+  Function(String error, String result) callback,
+);
+
+@JS()
+external RSAVerifyPSS(
+  String signature,
+  String message,
+  String hashName,
+  String pkcs12,
+  String passphrase,
+  Function(String error, bool result) callback,
+);
+
+@JS()
+external RSAVerifyPKCS1v15(
+  String signature,
+  String message,
+  String hashName,
+  String pkcs12,
+  String passphrase,
+  Function(String error, bool result) callback,
 );
 
 @JS()
@@ -14,4 +82,16 @@ external RSAHash(
   String message,
   String hash,
   Function(String error, String result) callback,
+);
+
+@JS()
+external RSABase64(
+  String message,
+  Function(String error, String result) callback,
+);
+
+@JS()
+external RSAGenerate(
+  int bits,
+  Function(String error, dynamic result) callback,
 );
--- lib/web/rsa_web.dart
+++ lib/web/rsa_web.dart
@@ -52,20 +52,223 @@ class RsaPlugin {
     await loadInstance();
 
     switch (call.method) {
+      case 'decryptOAEP':
+        return await decryptOAEP(
+          call.arguments['message'],
+          call.arguments['label'],
+          call.arguments['hashName'],
+          call.arguments['pkcs12'],
+          call.arguments['passphrase'],
+        );
+      case 'decryptPKCS1v15':
+        return await decryptPKCS1v15(
+          call.arguments['message'],
+          call.arguments['pkcs12'],
+          call.arguments['passphrase'],
+        );
+      case 'encryptOAEP':
+        return await encryptOAEP(
+          call.arguments['message'],
+          call.arguments['label'],
+          call.arguments['hashName'],
+          call.arguments['pkcs12'],
+          call.arguments['passphrase'],
+        );
+      case 'encryptPKCS1v15':
+        return await encryptPKCS1v15(
+          call.arguments['message'],
+          call.arguments['pkcs12'],
+          call.arguments['passphrase'],
+        );
+      case 'signPSS':
+        return await signPSS(
+          call.arguments['message'],
+          call.arguments['hashName'],
+          call.arguments['pkcs12'],
+          call.arguments['passphrase'],
+        );
+      case 'signPKCS1v15':
+        return await signPKCS1v15(
+          call.arguments['message'],
+          call.arguments['hashName'],
+          call.arguments['pkcs12'],
+          call.arguments['passphrase'],
+        );
+      case 'verifyPSS':
+        return await verifyPSS(
+          call.arguments['signature'],
+          call.arguments['message'],
+          call.arguments['hashName'],
+          call.arguments['pkcs12'],
+          call.arguments['passphrase'],
+        );
+      case 'verifyPKCS1v15':
+        return await verifyPKCS1v15(
+          call.arguments['signature'],
+          call.arguments['message'],
+          call.arguments['hashName'],
+          call.arguments['pkcs12'],
+          call.arguments['passphrase'],
+        );
       case 'hash':
-        return await _hash(call.arguments['message'], call.arguments['name']);
+        return await hash(
+          call.arguments['message'],
+          call.arguments['name'],
+        );
+      case 'base64':
+        return await base64(
+          call.arguments['message'],
+        );
       case 'generate':
-        return await _generate(call.arguments['bits']);
+        return await generate(
+          call.arguments['bits'],
+        );
       default:
         throw PlatformException(
             code: 'Unimplemented',
-            details: "The url_launcher plugin for web doesn't implement "
+            details: "The fast_rsa_web plugin for web doesn't implement "
                 "the method '${call.method}'");
     }
   }
 
-  Future<dynamic> _generate(int bits) async {
-    var completer = new Completer();
+  Future<String> decryptOAEP(String message, String label, String hashName,
+      String pkcs12, String passphrase) async {
+    var completer = new Completer<String>();
+    RSADecryptOAEP(message, label, hashName, pkcs12, passphrase,
+        allowInterop((String error, String result) {
+      if (error != null && error != "") {
+        completer.completeError(error);
+        return;
+      }
+      completer.complete(result);
+    }));
+    return completer.future;
+  }
+
+  Future<String> decryptPKCS1v15(
+      String message, String pkcs12, String passphrase) async {
+    var completer = new Completer<String>();
+    RSADecryptPKCS1v15(message, pkcs12, passphrase,
+        allowInterop((String error, String result) {
+      if (error != null && error != "") {
+        completer.completeError(error);
+        return;
+      }
+      completer.complete(result);
+    }));
+    return completer.future;
+  }
+
+  Future<String> encryptOAEP(String message, String label, String hashName,
+      String pkcs12, String passphrase) async {
+    var completer = new Completer<String>();
+    RSAEncryptOAEP(message, label, hashName, pkcs12, passphrase,
+        allowInterop((String error, String result) {
+      if (error != null && error != "") {
+        completer.completeError(error);
+        return;
+      }
+      completer.complete(result);
+    }));
+    return completer.future;
+  }
+
+  Future<String> encryptPKCS1v15(
+      String message, String pkcs12, String passphrase) async {
+    var completer = new Completer<String>();
+    RSAEncryptPKCS1v15(message, pkcs12, passphrase,
+        allowInterop((String error, String result) {
+      if (error != null && error != "") {
+        completer.completeError(error);
+        return;
+      }
+      completer.complete(result);
+    }));
+    return completer.future;
+  }
+
+  Future<String> signPSS(
+      String message, String hashName, String pkcs12, String passphrase) async {
+    var completer = new Completer<String>();
+    RSASignPSS(message, hashName, pkcs12, passphrase,
+        allowInterop((String error, String result) {
+      if (error != null && error != "") {
+        completer.completeError(error);
+        return;
+      }
+      completer.complete(result);
+    }));
+    return completer.future;
+  }
+
+  Future<String> signPKCS1v15(
+      String message, String hashName, String pkcs12, String passphrase) async {
+    var completer = new Completer<String>();
+    RSASignPKCS1v15(message, hashName, pkcs12, passphrase,
+        allowInterop((String error, String result) {
+      if (error != null && error != "") {
+        completer.completeError(error);
+        return;
+      }
+      completer.complete(result);
+    }));
+    return completer.future;
+  }
+
+  Future<bool> verifyPSS(String signature, String message, String hashName,
+      String pkcs12, String passphrase) async {
+    var completer = new Completer<bool>();
+    RSAVerifyPSS(signature, message, hashName, pkcs12, passphrase,
+        allowInterop((String error, bool result) {
+      if (error != null && error != "") {
+        completer.completeError(error);
+        return;
+      }
+      completer.complete(result);
+    }));
+    return completer.future;
+  }
+
+  Future<bool> verifyPKCS1v15(String signature, String message, String hashName,
+      String pkcs12, String passphrase) async {
+    var completer = new Completer<bool>();
+    RSAVerifyPKCS1v15(signature, message, hashName, pkcs12, passphrase,
+        allowInterop((String error, bool result) {
+      if (error != null && error != "") {
+        completer.completeError(error);
+        return;
+      }
+      completer.complete(result);
+    }));
+    return completer.future;
+  }
+
+  Future<String> hash(String message, String name) async {
+    var completer = new Completer<String>();
+    RSAHash(message, name, allowInterop((String error, String result) {
+      if (error != null && error != "") {
+        completer.completeError(error);
+        return;
+      }
+      completer.complete(result);
+    }));
+    return completer.future;
+  }
+
+  Future<String> base64(String message) async {
+    var completer = new Completer<String>();
+    RSABase64(message, allowInterop((String error, String result) {
+      if (error != null && error != "") {
+        completer.completeError(error);
+        return;
+      }
+      completer.complete(result);
+    }));
+    return completer.future;
+  }
+
+  Future<dynamic> generate(int bits) async {
+    var completer = new Completer<dynamic>();
     RSAGenerate(1024, allowInterop((String error, dynamic result) {
       if (error != null && error != "") {
         completer.completeError(error);
@@ -75,18 +278,6 @@ class RsaPlugin {
         "publicKey": result.publicKey,
         "privateKey": result.privateKey,
       });
-    }));
-    return completer.future;
-  }
-
-  Future<dynamic> _hash(String message, String name) async {
-    var completer = new Completer();
-    RSAHash(message, name, allowInterop((String error, String result) {
-      if (error != null && error != "") {
-        completer.completeError(error);
-        return;
-      }
-      completer.complete(result);
     }));
     return completer.future;
   }
