ccd98c8712ea094b743d85a43d02e53e81dbeb5d
Gerson Alexander Pardo Gamez <jerson@users.noreply.github.com>
2020-11-30 16:19:04 -0500

    Update upgrade_bridge_libs.sh

--- scripts/upgrade_bridge_libs.sh
+++ scripts/upgrade_bridge_libs.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+#######################################################
+# Usage:                                              #
+# ./upgrade_bridge_libs.sh                            #
+#                                                     #
+# or custom version:                                  #
+# VERSION=v0.1.1 ./upgrade_bridge_libs.sh             #
+#######################################################
 
 REPO="jerson/rsa-mobile"
 NAME="librsa_bridge"
@@ -6,6 +13,10 @@ PLATFORMS=("android" "darwin" "ios" "wasm" "linux" "windows")
 OUTPUT_DIRS=("android/src/main" "macos" "ios" "lib/web/assets" "linux/shared" "windows/shared")
 OUTPUT_SUB_DIRS=("" "" "" "" "./amd64" "./amd64")
 OUTPUT_STRIP_DIRS=(1 1 1 1 2 2)
+
+#######################################################
+# you shouldn't edit below this line                  #
+#######################################################
 
 
 echo "Get latest release"
@@ -15,7 +26,8 @@ get_version() {
   echo "$RELEASE_PAYLOAD" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'
 }
 
-VERSION=$(get_version $REPO)
+LATEST_VERSION=$(get_version $REPO)
+VERSION=${VERSION:-$LATEST_VERSION}
 
 echo "Using: $VERSION"
 echo "--------------------------------------------"
