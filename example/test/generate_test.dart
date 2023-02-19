5e37b873dc293fbcaf00c0c50321e314d3c9a795
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2023-02-18 19:26:52 -0500

    chore: updated versions and fixed unit test

--- example/test/generate_test.dart
+++ example/test/generate_test.dart
@@ -4,6 +4,7 @@ import 'package:fast_rsa/fast_rsa.dart';
 void main() {
   test('Generate Keypair', () async {
     var keyPair = await RSA.generate(2048);
-    expect(keyPair.publicKey, "here");
+    print(keyPair.privateKey);
+    expect(keyPair.publicKey!="", true);
   });
 }
