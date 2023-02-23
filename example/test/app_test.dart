3a578005e1f6ced00b3720f59562a4f8d50eb4de
Gerson Alexander Pardo Gamez <jerson@users.noreply.github.com>
2023-02-22 22:46:45 -0500

    feat: added linux ARM64 support (#50)

--- example/test/app_test.dart
+++ example/test/app_test.dart
@@ -3,7 +3,7 @@ import 'package:fast_rsa/fast_rsa.dart';
 
 void main() {
   test('Generate Keypair', () async {
-    var keyPair = await RSA.generate(2048);
+    var keyPair = await RSA.generate(1024);
     print(keyPair.privateKey);
     expect(keyPair.publicKey != "", true);
   });
